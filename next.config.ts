36c3a75f560ec0dc18d5e94e4da0d79ce9cbe973
Usman Ahmad <60046773+usmanh333@users.noreply.github.com>
2025-12-09 21:18:13 +0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,25 +1,16 @@
-// import type { NextConfig } from "next";
+import type { NextConfig } from "next";
 
-// const nextConfig: NextConfig = {
-//   /* config options here */
-// };
-
-// export default nextConfig;
-@type {import('next').NextConfig} */
-const nextConfig = {
+const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "example.com",
+        hostname: "**",
       },
-      {
-        protocol: "https",
-        hostname: "images.unsplash.com",
-      }
     ],
   },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
+
 
